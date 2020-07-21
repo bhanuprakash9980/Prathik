f3183c49840ad6c24cd8a746c8938cafd925e80c
bhanuprakash9980 <53945083+bhanuprakash9980@users.noreply.github.com>
2020-07-22 00:57:13 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 const mongoDBErrors=require('mongoose-mongodb-errors');
 mongoose.Promise=global.Promise;
 mongoose.plugin(mongoDBErrors);
-mongoose.connect("mongodb+srv://Bhanu_29:9980dontsteel@cluster0-pbefo.gcp.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true ,useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://username:password@cluster0-pbefo.gcp.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true ,useUnifiedTopology: true })
 .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
